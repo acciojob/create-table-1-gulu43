bf7aab1d6fdf33f95512548e412cb52acd42b7d1
gulu43 <145833927+gulu43@users.noreply.github.com>
2025-02-13 20:41:10 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,16 @@
 function insert_Row() {
     //Write your code here
-  
-  
+	let tb_val = document.getElementById('sampleTable');
+
+	//retriving and adding row to table
+	let row_val = document.createElement('tr');
+	tb_val.append(row_val);
+
+	//retriving and adding text and adding to row
+	let col_left_val = document.createElement('td');
+	col_left_val.textContent='New Cell1';
+	let col_right_val = document.createElement('td');
+	col_right_val.textContent='New Cell2';
+	
+	row_val.append(col_left_val,col_right_val);
 }
